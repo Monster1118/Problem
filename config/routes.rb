a535d28ab3ce110d88ecb9404785757793d32cb3
monster <1059105316@qq.com>
2018-12-18 21:26:06 +0800

    prob

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'foo/bar'
+  get 'foo/baz'
+  get 'pages/home'
+  get 'pages', to: 'pages#index'
+  get 'pages/help'
+  get 'pages/about'
+  root 'application#hello'
 end
